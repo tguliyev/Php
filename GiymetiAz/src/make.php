82db972dcac484588396e4646967beca56066246
tguliyev <tguliev9@gmail.com>
2022-07-12 16:53:42 +0400

    edit

--- GiymetiAz/src/make.php
+++ GiymetiAz/src/make.php
@@ -20,62 +20,40 @@
 
         // Get target menu links
         $links = scrapeContent($dom, $page, 'https://qiymeti.net', '//nav[contains(@class,"menu")]/ul/li/a');
-        // $page->navigate('https://qiymeti.net')->waitForNavigation();
-
-        // @$dom->loadHTML($page->getHtml(10000));
-        // $finder = new DOMXPath($dom);
-        // $links = $finder->query('//nav[contains(@class,"menu")]/ul/li/a');
         $menu_link = [];
         for ($i=0; $i < count($links); $i++) { 
             $menu_link[] = $links[$i]->attributes->getNamedItem('href')->nodeValue;
         }
-        // print_r($menu_link);
-        // echo "<br>";
-        // print_r($menu_id);
 
-
-        
         for ($i=0; $i < count($menu_id) ; $i++) { 
 
             // Get Makes
-            // $page->navigate($menu_link[$i])->waitForNavigation();
-    
-            // @$dom->loadHTML($page->getHtml(10000));
-            
-            // $finder = new DOMXPath($dom);
-            
-            // $makes = $finder->query('//li[contains(@class,"filter brands")]/div[contains(@class, "filter-content")]/div/label/a');
             $makes = scrapeContent($dom, $page, $menu_link[$i], '//li[contains(@class,"filter brands")]/div[contains(@class, "filter-content")]/div/label/a');
-            
             $sql = "INSERT IGNORE INTO `make` (`name`, `menu`) VALUES ";
             for ($j=0; $j < count($makes); $j++) {
+                if ($makes[$j]->nodeValue == "iPhone" || $makes[$j]->nodeValue == "iPad") $makes[$j]->nodeValue = "Apple";
                 $sql .= "('".addslashes($makes[$j]->nodeValue)."',".$menu_id[$i].") ";
                 $sql .= ($j == count($makes) - 1) ?  "" : ",";
             }
-            echo $sql;
-            echo "<br>";
             $conn->query($sql);
 
 
             // Get Items Info
             $item_pages = 5;
             for ($j=0; $j < $item_pages; $j++) {
+                
                 $items_link = $menu_link[$i] . "page/".($j + 1);
                 
-                // $page->navigate($items_link)->waitForNavigation();
-                // @$dom->loadHTML($page->getHtml(20000));
-                // $finder = new DOMXPath($dom);
-                
-                // $items = $finder->query('//div[@class="products"]');
                 $items = scrapeContent($dom, $page, $items_link, '//div[@class="products"]');
-                print_r($items);
                 echo $items_link;
-                print_r($items[0]->childNodes);
 
                 $items = $items[0]->childNodes;
 
                 for ($k=0; $k < count($items) - 1; $k++) { 
-                    echo $items[$k]->childNodes[0]->childNodes[0]->attributes->getNamedItem('href')->nodeValue;
+                    echo "<br>";
+                    $link = $items[$k]->childNodes[0]->childNodes[0]->attributes->getNamedItem('href')->nodeValue;
+                    echo $link;
+                    $item_price = scrapeContent($dom, $page, "", "" );
                     echo "<br>";
                     echo $items[$k]->childNodes[0]->childNodes[0]->childNodes[0]->attributes->getNamedItem('src')->nodeValue;
                     echo "<br>";
@@ -100,5 +78,15 @@
         echo $e;
     } finally {
         $browser->close();
+    }
+
+    function getMenuId($conn) {
+        $sql = "SELECT * FROM `menu` ORDER BY `order`";
+        $result = $conn->query($sql);
+        $menu_id =[];
+        while ($row = $result->fetch_assoc()) {
+            $menu_id[] = $row['id'];
+        }
+        return $menu_id;
     }
 ?>
