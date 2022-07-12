74fc9fe2e26c3da0ab2ae34be3653caf8aabcd4b
tguliyev <tguliev9@gmail.com>
2022-07-12 17:32:13 +0400

    edit

--- GiymetiAz/src/make.php
+++ GiymetiAz/src/make.php
@@ -3,6 +3,8 @@
     require_once "function.php";
     require __DIR__.'/vendor/autoload.php';
     use HeadlessChromium\BrowserFactory;
+
+    $item_pages = 5;
     
     $browserFactory = new BrowserFactory();
     $browser = $browserFactory->createBrowser();
@@ -11,56 +13,61 @@
 
     try {
         // Get menu ids
-        $sql = "SELECT * FROM `menu` ORDER BY `order`";
-        $result = $conn->query($sql);
-        $menu_id =[];
-        while ($row = $result->fetch_assoc()) {
-            $menu_id[] = $row['id'];
-        }
-
+        $menu_id = getMenuId($conn);
         // Get target menu links
-        $links = scrapeContent($dom, $page, 'https://qiymeti.net', '//nav[contains(@class,"menu")]/ul/li/a');
-        $menu_link = [];
-        for ($i=0; $i < count($links); $i++) { 
-            $menu_link[] = $links[$i]->attributes->getNamedItem('href')->nodeValue;
-        }
+        $menu_link = getMenuLink($dom, $page);
 
         for ($i=0; $i < count($menu_id) ; $i++) { 
 
-            // Get Makes
-            $makes = scrapeContent($dom, $page, $menu_link[$i], '//li[contains(@class,"filter brands")]/div[contains(@class, "filter-content")]/div/label/a');
-            $sql = "INSERT IGNORE INTO `make` (`name`, `menu`) VALUES ";
-            for ($j=0; $j < count($makes); $j++) {
-                if ($makes[$j]->nodeValue == "iPhone" || $makes[$j]->nodeValue == "iPad") $makes[$j]->nodeValue = "Apple";
-                $sql .= "('".addslashes($makes[$j]->nodeValue)."',".$menu_id[$i].") ";
-                $sql .= ($j == count($makes) - 1) ?  "" : ",";
-            }
-            $conn->query($sql);
+            // Get and Add all Makes
+            // $make = scrapeContent($dom, $page, $menu_link[$i], '//li[contains(@class,"filter brands")]/div[contains(@class, "filter-content")]/div/label/a');
+            // $sql = "INSERT IGNORE INTO `make` (`name`, `menu`) VALUES ";
+            // for ($j=0; $j < count($make); $j++) {
+            //     if ($make[$j]->nodeValue == "iPhone" || $make[$j]->nodeValue == "iPad") $make[$j]->nodeValue = "Apple";
+            //     $sql .= "('".addslashes($make[$j]->nodeValue)."',".$menu_id[$i].") ";
+            //     $sql .= ($j == count($make) - 1) ?  "" : ",";
+            // }
+            // $conn->query($sql);
+            addMake($dom, $page, $conn, $menu_link, $menu_id, $i);
 
 
             // Get Items Info
-            $item_pages = 5;
             for ($j=0; $j < $item_pages; $j++) {
                 
-                $items_link = $menu_link[$i] . "page/".($j + 1);
+                $item_page_link = $menu_link[$i] . "page/".($j + 1);
                 
-                $items = scrapeContent($dom, $page, $items_link, '//div[@class="products"]');
-                echo $items_link;
+                $item = scrapeContent($dom, $page, $item_page_link, '//div[@class="products"]');
+                echo $item_page_link;
+                $item = $item[0]->childNodes;
 
-                $items = $items[0]->childNodes;
+                for ($k=0; $k < count($item) - 1; $k++) { 
+                    echo "<br>";
+                    $item_link = $item[$k]->childNodes[0]->childNodes[0]->attributes->getNamedItem('href')->nodeValue;
+                    // echo $item_link;
+                    $item_price_page = scrapeContent($dom, $page, $item_link, '//div[@class="prices-wrapper"]');
+                    $item_price_page = $item_price_page[0]->childNodes;
+                    
+                    // description
+                    $item_desc = $item_price_page[1]->nodeValue;
 
-                for ($k=0; $k < count($items) - 1; $k++) { 
+                    $price_list = $item_price_page[6]->childNodes;
+                    for ($p=1; $p < count($price_list); $p++) { 
+                        
+                    }
+                    
+                    
+                    
                     echo "<br>";
-                    $link = $items[$k]->childNodes[0]->childNodes[0]->attributes->getNamedItem('href')->nodeValue;
-                    echo $link;
-                    $item_price = scrapeContent($dom, $page, "", "" );
+                    $item_image = $item[$k]->childNodes[0]->childNodes[0]->childNodes[0]->attributes->getNamedItem('src')->nodeValue;
+                    echo $item_image;
+
                     echo "<br>";
-                    echo $items[$k]->childNodes[0]->childNodes[0]->childNodes[0]->attributes->getNamedItem('src')->nodeValue;
+                    $item_name = $item[$k]->childNodes[1]->childNodes[0]->nodeValue;
+                    echo $item_name;
+
                     echo "<br>";
-                    echo $items[$k]->childNodes[1]->childNodes[0]->nodeValue;
-                    echo "<br>";
-                    if ($items[$k]->childNodes[2]->attributes->getNamedItem('class')->nodeValue == 'min-price') {
-                        echo intval(str_replace(' ', '', $items[$k]->childNodes[2]->childNodes[0]->nodeValue));
+                    if ($item[$k]->childNodes[2]->attributes->getNamedItem('class')->nodeValue == 'min-price') {
+                        echo intval(str_replace(' ', '', $item[$k]->childNodes[2]->childNodes[0]->nodeValue));
                     }
                     echo "<br>";
                     echo "<br>";
@@ -73,9 +80,8 @@
 
     } catch (Exception $e) {
         echo "<br>";
-        echo "Error";
-        echo "<br>";
         echo $e;
+        echo "<br>";
     } finally {
         $browser->close();
     }
@@ -89,4 +95,25 @@
         }
         return $menu_id;
     }
+
+    function getMenuLink($dom, $page) {
+        $link = scrapeContent($dom, $page, 'https://qiymeti.net', '//nav[contains(@class,"menu")]/ul/li/a');
+        $menu_link = [];
+        for ($i=0; $i < count($link); $i++) { 
+            $menu_link[] = $link[$i]->attributes->getNamedItem('href')->nodeValue;
+        }
+        return $menu_link;
+    }
+
+    function addMake($dom, $page, $conn, $menu_link, $menu_id, $index) {
+        $make = scrapeContent($dom, $page, $menu_link[$index], '//li[contains(@class,"filter brands")]/div[contains(@class, "filter-content")]/div/label/a');
+        $sql = "INSERT IGNORE INTO `make` (`name`, `menu`) VALUES ";
+        for ($j=0; $j < count($make); $j++) {
+            if ($make[$j]->nodeValue == "iPhone" || $make[$j]->nodeValue == "iPad") $make[$j]->nodeValue = "Apple";
+            $sql .= "('".addslashes($make[$j]->nodeValue)."',".$menu_id[$index].") ";
+            $sql .= ($j == count($make) - 1) ?  "" : ",";
+        }
+        $conn->query($sql);
+    }
+
 ?>
